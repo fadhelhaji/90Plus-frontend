4ef17c4bb82638559596446f3a5376a3f2738dd1
Fadhel Haji <fadhelmh@hotmail.com>
2026-01-24 19:05:09 +0300

    Project init

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
+import App from './App.jsx'
+import { UserProvider } from './Contexts/UserContext.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <UserProvider>
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
+    </UserProvider>
+  </StrictMode>,
+)
